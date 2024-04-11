24ede074237088dd951e49f7f7e9d51853775dad
Ezequiel Santos <ezequiel.santos@miniclip.com>
2024-04-11 09:29:55 +0100

    bump to version 0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.5'
+version = '0.6'
 
 setup(
     name='scholarly_publications',
